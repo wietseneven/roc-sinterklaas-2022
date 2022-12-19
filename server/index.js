5dc72216aee959de685a9e2d006fe7a5ca261961
Wietse Neven <hello@wietseneven.com>
2022-12-19 20:46:57 +0100

    whitelist live url for cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: ["http://localhost:8080", "https://sinterklaas-2022.mediavormgever.online"],
   },
 });
 
